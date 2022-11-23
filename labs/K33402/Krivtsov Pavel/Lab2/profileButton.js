6f9193d1b5a16800ed2d01f5fed877aa4bdcca5b
Krivtsov Pavel <pkrivtsov6@gmail.com>
2022-11-23 12:48:53 +0300

    refactor: add russian language

--- labs/K33402/Krivtsov Pavel/Lab2/profileButton.js
+++ labs/K33402/Krivtsov Pavel/Lab2/profileButton.js
@@ -2,31 +2,31 @@ function getButtonHTML() {
     if (localStorage.accessToken) {
         return `
             <a href="./profile.html" class="btn btn-orange btn-md" role="button">
-                Profile
+                Профиль
             </a>
 
             <button type="button" class="btn btn-md btn-orange dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
                 <span class="visually-hidden">Toggle Dropdown</span>
             </button>
             <ul class="dropdown-menu dropdown-menu-dark">
-                <li><a class="dropdown-item" href="./profile.html">Profile</a></li>
+                <li><a class="dropdown-item" href="./profile.html">Профиль</a></li>
                 <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" onclick="logout()">Sign out</a></li>
+                <li><a class="dropdown-item" onclick="logout()">Выйти</a></li>
             </ul>
         `
     } else {
         return `
             <a href="./signup.html" class="btn btn-orange btn-md" role="button">
-                Profile
+                Профиль
             </a>
 
             <button type="button" class="btn btn-md btn-orange dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
                 <span class="visually-hidden">Toggle Dropdown</span>
             </button>
             <ul class="dropdown-menu dropdown-menu-dark">
-                <li><a class="dropdown-item" href="./signin.html">Sign In</a></li>
+                <li><a class="dropdown-item" href="./signin.html">Войти</a></li>
                 <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="./signup.html">Sign Up</a></li>
+                <li><a class="dropdown-item" href="./signup.html">Зарегистрироваться</a></li>
             </ul>
         `
     }
