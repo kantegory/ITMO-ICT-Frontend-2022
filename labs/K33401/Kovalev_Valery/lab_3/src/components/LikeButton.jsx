cef5d8253d649320fdf972a2374f9a8c91e970c6
kovalev-vxx <kovalev.vxx@gmail.com>
2022-11-28 01:55:43 +0300

    hm4

--- labs/K33401/Kovalev_Valery/lab_3/src/components/LikeButton.jsx
+++ labs/K33401/Kovalev_Valery/lab_3/src/components/LikeButton.jsx
@@ -6,8 +6,11 @@ import {FaHeart, FaHeartBroken} from "react-icons/fa"
 
 const LikeButton = ({style, className, onClick}) => {
     return (
-        <Button aria-label="like photo" onClick={onClick} className={className} variant="secondary" style={{borderRadius:"50%", width:"50px", height:"50px", ...style}}>
-            <FaHeart/>
+        <Button aria-label="like photo" onClick={onClick} className={className + "d-flex justify-content-center"} variant="secondary" style={{borderRadius:"50%", width:"50px", height:"50px", ...style}}>
+            {/*<FaHeart/>*/}
+            <svg style={{width:"25px", height:"25px", stroke:"var(--background-color)"}}>
+                <use xlinkHref="#star"></use>
+            </svg>
         </Button>
     );
 };
