70bd86a0e435e4949f88b4d6738f84c7f4c0368d
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2023-01-18 23:43:45 +0300

    :rocket: [feat] set some more important settings

--- labs/K33401/Kobelev/LR3/ScenariosAndShots/src/main.js
+++ labs/K33401/Kobelev/LR3/ScenariosAndShots/src/main.js
@@ -1,14 +1,16 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import store from './stores'
 
 import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(store)
 app.use(router)
 
 app.mount('#app')
--- labs/K33401/Kobelev/LR3/ScenariosAndShots/src/router/index.js
+++ labs/K33401/Kobelev/LR3/ScenariosAndShots/src/router/index.js
@@ -1,23 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {createRouter, createWebHistory} from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    mode: "history",
+    routes: [
+        {
+            path: '/scenarios',
+            name: 'scenarios',
+            component: () => import('../views/HomeView.vue')
+        },
+        {
+            path: '/scenarios/:id',
+            name: 'scenario',
+            component: () => import('../views/ScenarioView.vue'),
+            props: true
+        }
+    ]
 })
 
 export default router
