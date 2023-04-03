27ea7d4c6c8eaa1f90b01373af9a2c711214e605
Deidaras <71378094+Deidaras@users.noreply.github.com>
2023-04-03 04:13:31 +0300

    done

---
+++ labs/K33401/MukhinaYuliya/3lab/vue-progect/src/components/Footer.vue
@@ -0,0 +1,52 @@
+<template>
+	<footer class="text-center text-white footer-color" style="background-color:#b2222289;">
+			<div class="container pt-4">
+				<section class="mb-4">
+					<a
+						class="btn btn-link btn-floating btn-lg text-dark m-1"
+						href="#!"
+						role="button"
+						data-mdb-ripple-color="dark"
+						><svg class="icon" xmlns="http://www.w3.org/2000/svg">
+							<use xlink:href = "#telegr"></use>
+						</svg>
+					</a>
+					<a
+						class="btn btn-link btn-floating btn-lg text-dark m-1"
+						href="#!"
+						role="button"
+						data-mdb-ripple-color="dark"
+						><svg class="icon" xmlns="http://www.w3.org/2000/svg">
+							<use xlink:href = "#inst"></use>
+						</svg></a>
+					<a
+						class="btn btn-link btn-floating btn-lg text-dark m-1"
+						href="#!"
+						role="button"
+						data-mdb-ripple-color="dark"
+						><svg class="icon" xmlns="http://www.w3.org/2000/svg">
+							<use xlink:href = "#twi"></use>
+						</svg></a>
+					<a
+						class="btn btn-link btn-floating btn-lg text-dark m-1"
+						href="#!"
+						role="button"
+						data-mdb-ripple-color="dark"
+						><svg class="icon" xmlns="http://www.w3.org/2000/svg">
+							<use xlink:href = "#facebook"></use>
+						</svg></a>
+				</section>
+			</div>
+			<div class="text-center text-dark p-3 footer-text">
+				© 2022 Copyright:
+				<a class="text-dark footer-text" href="http://t.me/rassvet2002">Kulagina Svetlava</a>
+			</div>
+		</footer>
+</template>
+
+<script>
+export default {
+	name: 'FooterBlock'
+}
+</script>
+
